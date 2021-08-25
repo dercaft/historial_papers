53bc07cb7810e4f752f3de0341d3682c69a3e1de
dercaft <wuyuhang2073@gmail.com>
2021-08-25 20:28:16 +0800

    Add script to form text files

---
+++ html2txt.py
@@ -0,0 +1,27 @@
+import csv,re,os
+from generate import EEBO_Controller,EEBO_Parser
+from generate import OLL_Parser,OLL_Controller
+
+if __name__=="__main__":
+    lister=[]
+    with open("./papers.csv","r", encoding="utf-8") as f:
+        read=csv.reader(f)
+        lister=[i for i in read]
+    for index,i in enumerate(lister):
+        title,path=i[2],i[3]
+
+        if i[1]=="EEBO":
+            parser=EEBO_Parser()
+            controller=EEBO_Controller(path,parser)
+        elif i[1]=="OLL":
+            parser=OLL_Parser()
+            controller=OLL_Controller(path,parser)
+        else: continue
+        controller.explore(controller.container)
+        content=parser.article
+        for i,t in enumerate(content):
+            content[i]=re.sub("[ ]+"," ",t)
+        name=path.split("/")[-1].split(".")[0]
+        with open(os.path.join("./txt",name+".txt") ,"w+",encoding="utf-8") as f:
+            for line in content:
+                f.write(line)     
